38cedd6c46d7dd7abeb2c808ca5552874abde16b
imhoiamcat <masha.a.ushakova@gmail.com>
2025-01-18 20:30:31 +0100

    gpt ws

--- GPT/main.py
+++ GPT/main.py
@@ -1,34 +1,47 @@
+import base64
+from openai import OpenAI, api_key
 import os
 
-from openai import OpenAI
+from samples.image_test import base64_image
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
 
 
-# def sendGPTRequest(image, address: str):
-def sendGPTRequest(image):
-    assistant_id = "asst_gShRAai0FxG4sHmtfdlS87dx"
-    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+def process_image(image):
+    api_key = os.getenv("OPENAI_API_KEY")
+    client = OpenAI(
 
-    thread = client.beta.threads.create()
+        base_url = "https://api.openai.com/v1/"
+    )
 
-    # message = {
-    #    "role": "user",
-    #    "content": "Say this is a test",
-    # }
-    response = client.chat.completions.create(
+    file = open("prompt.txt", 'r')
+    prompt = file.read()
+
+    completion = client.chat.completions.create(
         model="gpt-4o",
         messages=[
             {
                 "role": "user",
                 "content": [
-                    {"type": "text", "text": "Describe image"},
+                    {"type": "text", "text": prompt},
                     {
                         "type": "image_url",
-                        "image_url": {"url": f"{image}"},
+                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
                     },
                 ],
             }
         ],
+        temperature=0.5
     )
 
+    for chunk in completion:
+        if len(chunk) > 0 and chunk[0] == 'choices':
+            print(chunk[1][0].message.content, end='')
+
+
 if __name__ == "__main__":
-    sendGPTRequest("../bsa.jpg")
+    image_path = "../bsa.jpg"
+    image = encode_image(image_path)
+    process_image(image)
