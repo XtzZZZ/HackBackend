3d5f65f34db6a25a9b0d2fef0a6824de160d714b
XtzZZZ <maxim.savochkin120@gmail.com>
2025-01-18 19:07:15 +0100

    reqs

--- GPT/main.py
+++ GPT/main.py
@@ -1,9 +1,32 @@
+import json
 import os
-
+import websocket
 from openai import OpenAI
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-
-# def sendGPTRequest(image, address: str):
+# url = "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-12-17"
+# headers = [
+#     "Authorization: Bearer " + OPENAI_API_KEY,
+#     "OpenAI-Beta: realtime=v1"
+# ]
+#
+# def on_open(ws):
+#     print("Connected to server.")
+#
+# def on_message(ws, message):
+#     data = json.loads(message)
+#     print("Received event:", json.dumps(data, indent=2))
+#
+# ws = websocket.WebSocketApp(
+#     url,
+#     header=headers,
+#     on_open=on_open,
+#     on_message=on_message,
+# )
+#
+# ws.run_forever()
+#
+# # def sendGPTRequest(image, address: str):
 
 def process_image(image):
     assistant_id = "asst_gShRAai0FxG4sHmtfdlS87dx"
@@ -33,4 +56,5 @@ def process_image(image):
     )
 
 if __name__ == "__main__":
-    sendGPTRequest("../bsa.jpg")
+    pass
+    # sendGPTRequest("../bsa.jpg")
--- HackBackend/settings.py
+++ HackBackend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +27,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = "/static/"
+
+# ✅ Add this line for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# ✅ Ensure static files are correctly found
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Application definition
 
