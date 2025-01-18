467ccf4ff20712f10f8f0e60d29ae260f00e2ac1
imhoiamcat <masha.a.ushakova@gmail.com>
2025-01-18 17:18:30 +0100

    gpt ws

--- GPT/main.py
+++ GPT/main.py
@@ -1,11 +1,35 @@
+import os
+
 from openai import OpenAI
 
-def sendGPTRequest(image, address: str):
+
+# def sendGPTRequest(image, address: str):
+def sendGPTRequest(image):
     assistant_id = "asst_gShRAai0FxG4sHmtfdlS87dx"
-    client = OpenAI()
+    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
     thread = client.beta.threads.create()
 
-    message = {
+    # message = {
+    #    "role": "user",
+    #    "content": "Say this is a test",
+    # }
 
-    }
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Describe image"},
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"{image}"},
+                    },
+                ],
+            }
+        ],
+    )
+
+if __name__ == "__main__":
+    sendGPTRequest("../bsa.jpg")
